74f17078529d1ada33d3fbfc6b15a71baff24715
Fabio Formosa <fabio.formosa@dedagroup.it>
2018-09-28 15:46:15 +0200

    applied @EnableMetamorphosisConversions also in test context

--- metamorphosis/src/test/java/dev/metamorphosis/ConversionTestConfig.java
+++ metamorphosis/src/test/java/dev/metamorphosis/ConversionTestConfig.java
@@ -1,10 +1,9 @@
 package dev.metamorphosis;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = { "dev.metamorphosis" })
+@EnableMetamorphosisConversions
 public class ConversionTestConfig {
 
 }
