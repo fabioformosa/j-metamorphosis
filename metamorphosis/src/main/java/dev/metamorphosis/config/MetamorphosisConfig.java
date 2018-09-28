1aad6ab9741e2eadbf42dc07856e91f46e433b37
Fabio Formosa <fabio.formosa@dedagroup.it>
2018-09-28 17:26:46 +0200

    added condition to avoid conversionService creation if it exists

--- metamorphosis/src/main/java/dev/metamorphosis/config/MetamorphosisConfig.java
+++ metamorphosis/src/main/java/dev/metamorphosis/config/MetamorphosisConfig.java
@@ -2,6 +2,7 @@ package dev.metamorphosis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.core.convert.ConversionService;
@@ -10,6 +11,7 @@ import org.springframework.core.convert.ConversionService;
 @ComponentScan(basePackages = { "dev.metamorphosis" })
 public class MetamorphosisConfig {
 
+  @Conditional(value = ConversionServiceCreationCondition.class)
   @Bean(name = "conversionService")
   public ConversionService getConversionService() {
     ConversionServiceFactoryBean bean = new ConversionServiceFactoryBean();
